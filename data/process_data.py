a12e3cd132f375ad58bfe8d355ece4b85e04341b
saurav29singh <108488386+saurav29singh@users.noreply.github.com>
2022-09-05 15:20:32 +0200

    Updated pipeline with Suggested comment

--- data/process_data.py
+++ data/process_data.py
@@ -64,7 +64,7 @@ def save_data(df, database_filename):
         database_filename -> database file (.db) destination path
     """
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('df', engine, index=False)
+    df.to_sql('df', engine, index=False, if_exists='replace')
     pass  
 
 

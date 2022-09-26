37784117a7d2e28c006753d9bfa1f7b0eb4f2d7d
saurav29singh <108488386+saurav29singh@users.noreply.github.com>
2022-09-26 18:11:44 +0200

    Updated the process_data.py as per suggestion.

--- data/process_data.py
+++ data/process_data.py
@@ -16,11 +16,12 @@ import sys
 import numpy as np
 import pandas as pd
 from sqlalchemy import create_engine
- 
+
+
 def load_data(messages_filepath, categories_filepath):
     """
     Load Data function
-    
+
     Arguments:
         messages_filepath -> path to messages csv file
         categories_filepath -> path to categories csv file
@@ -29,49 +30,64 @@ def load_data(messages_filepath, categories_filepath):
     """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
-    df = pd.merge(messages,categories,on='id')
-    return df 
+    df = pd.merge(messages, categories, on="id")
+    return df
+
 
 def clean_data(df):
     """
     Clean Data function
-    
+
     Arguments:
         df -> raw data Pandas DataFrame
     Outputs:
         df -> clean data Pandas DataFrame
     """
-    categories = df.categories.str.split(pat=';',expand=True)
-    firstrow = categories.iloc[0,:]
-    category_colnames = firstrow.apply(lambda x:x[:-2])
+    categories = df['categories'].str.split(";", expand=True)
+    # select the first row of the categories dataframe
+    row = categories.loc[0,]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x.split("-")[0])
     categories.columns = category_colnames
     for column in categories:
-        categories[column] = categories[column].str[-1]
-        categories[column] = categories[column].astype(np.int)
-    df = df.drop('categories',axis=1)
-    df = pd.concat([df,categories],axis=1)
-    #df = pd.concat([df,pd.get_dummies(df.genre)],axis=1)
-    #df = df.drop(['genre','social'],axis=1) 
-    df = df.drop_duplicates()
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(
+            lambda x: x.split("-")[-1])
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype('int')
+    # drop the original categories column from `df`
+    df.drop('categories', inplace=True, axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    # df = pd.concat([df,pd.get_dummies(df.genre)],axis=1)
+    # df = df.drop(['genre','social'],axis=1)
+    # drop duplicates
+    df = df.drop_duplicates(subset=['message'])
     return df
+
 
 def save_data(df, database_filename):
     """
     Save Data function
-    
+
     Arguments:
         df -> Clean data Pandas DataFrame
         database_filename -> database file (.db) destination path
     """
-    engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('df', engine, index=False, if_exists='replace')
-    pass  
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('df', engine, index=False)
+    pass
 
 
 def main():
     """
     Main Data Processing function
-    
+
     This function implement the ETL pipeline:
         1) Data extraction from .csv
         2) Data cleaning and pre-processing
@@ -88,18 +104,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
               'DisasterResponse.db')
 
 
